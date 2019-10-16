78844f6b646aabe6e99084234e98779bd909e53b
lideo <1573567+lideo@users.noreply.github.com>
2019-10-16 12:19:19 +0800

    Add project update views and implement controller functions.

--- controllers/projectController.js
+++ controllers/projectController.js
@@ -2,8 +2,8 @@ var Project = require('../models/project');
 var User = require('../models/user');
 
 var async = require('async');
-const { body, validationResult } = require('express-validator/check');
-const { sanitizeBody } = require('express-validator/filter');
+const { body, validationResult } = require('express-validator');
+const { sanitizeBody } = require('express-validator');
 
 exports.index = function(req, res) {
 
@@ -137,11 +137,56 @@ exports.project_delete_post = function(req, res, next) {
 };
 
 // Display project update form on GET.
-exports.project_update_get = function(req, res) {
-  res.send('NOT IMPLEMENTED: Project update GET');
+exports.project_update_get = function(req, res, next) {
+  async.parallel({
+    project: function(callback) {
+      Project.findById(req.params.id).exec(callback);
+    }
+  }, function(err, results) {
+    if (err) { return next(err); }
+    if (results.project == null) {
+      const err = new Error('Project not found.');
+      err.status = 404;
+      return next(err);
+    }
+    res.render('project_form', {
+      title: 'Update Project',
+      project: results.project
+    });
+  });
 };
 
 // Handle project update on POST.
-exports.project_update_post = function(req, res) {
-  res.send('NOT IMPLEMENTED: Project update POST');
-};
+exports.project_update_post = [
+  body('name', 'Name must not be empty.')
+    .isLength({ min: 1, max: 100 })
+    .trim(),
+
+    sanitizeBody('*').escape(),
+
+    (req, res, next) => {
+      const errors = validationResult(req);
+      const project = new Project({
+        name: req.body.name,
+        user: req.body.user,
+        html_code: req.body.html_code,
+        css_code: req.body.css_code,
+        js_code: req.body.js_code,
+        _id: req.params.id //This is required, or a new ID will be assigned!
+      });
+
+      if (!errors.isEmpty()) {
+        res.render('project_form', {
+          name: 'Create Project',
+          project: project,
+          errors: errors.array()
+        });
+        return;
+      } else {
+        Project.findByIdAndUpdate(req.params.id, project, {}, function(err, theproject) {
+          if (err) { return next(err); }
+          res.redirect(theproject.url);
+        });
+      }
+    }
+];
