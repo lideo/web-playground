8587606520467e8eae574a89700a96e3e14c92f5
lideo <1573567+lideo@users.noreply.github.com>
2019-10-31 10:31:31 +0800

    misc

--- app.js
+++ app.js
@@ -31,7 +31,7 @@ app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'pug');
 
 app.use(compression()); //Compress all routes
-app.use(helmet());
+app.use(helmet()); // Security
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
